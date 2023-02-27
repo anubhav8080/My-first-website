6c3f9b319afaf6d21bbf60fa5ff42a13125327f5
anubhav <anubhav8080@gmail.com>
2023-02-27 17:53:41 -0500

    database connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports =
  {
- "URI":"mongodb+srv://anubhav8080:anubhav2404@@cluster0.mpbedcl.mongodb.net"
+ "URI":"mongodb+srv://anubhav8080:qG6UvifzmkLtvv8Y@cluster0.mpbedcl.mongodb.net/?retryWrites=true&w=majority"
 }
